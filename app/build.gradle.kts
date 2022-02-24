8c499fe262657350a897e51307643365e841cc80
LauranEkeren <58422302+LauranEkeren@users.noreply.github.com>
2022-02-24 16:12:59 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id "org.sonarqube" version "3.3"
+    id("org.sonarqube") version "3.3"
 }
 
 repositories {
